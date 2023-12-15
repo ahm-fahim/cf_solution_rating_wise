a5eb09aafe87d52a6fd65cff0cb527e6d7e0b973
ahm-fahim <contactahmfahim@gmail.com>
2023-12-15 07:07:21 +0600

    solved succesfully

---
+++ 800/61_A_Young_Physicist.cpp
@@ -0,0 +1,55 @@
+/****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****\
+ |  Problems! Problems! Problems! I Don't Like It, I Avoid.             |
+ |  But Problems Likes Me! I can't Avoid!                               |
+ |                                                                      |
+ |  A.H.M. Fahim Morshed                                                |
+ |                                                                      |
+ |  ```Is a competitive programming enthusiast who is deeply            |
+ |  passionate about coding. Hailing from Bogura, Bangladesh,           |
+ |  his coding journey started at a young age, driven by a              |
+ |  fascination for solving intricate problems.                         |
+ |                                                                      |
+ |  Fahim has excelled in competitive programming, consistently         |
+ |  ranking among the top participants in various                       |
+ |  contests.Beyond competitions, he shares his                         |
+ |  expertise by mentoring and assisting fellow                         |
+ |  programmers on online forums.                                       |
+ |                                                                      |
+ |  His dedication to self-improvement, willingness to embrace new      |
+ |  challenges, and effective teamwork make him a valuable              |
+ |  asset to any project.                                               |
+ |                                                                      |
+ |  Fahim's love for coding, combined with his diverse interests,       |
+ |  reflects his well-rounded personality, making him an inspiring      |
+ |  figure in the programming community at Southeast University.```     |
+ |                                                                      |
+ |  Github   - ahm-fahim                                                |
+ |  Linkedin - https://www.linkedin.com/in/md-fahim-morshed-5b2126233/  |
+ |  Gmail    - contactahmfahim@gmail.com                                |
+\****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl '\n';
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin >> n;
+
+    int x = 0, y =0, z =0;
+
+    while (n--)
+    {
+        int xi, yi, zi;
+        cin >> xi >> yi >> zi;
+
+        x += (xi),y += (yi),z += (zi);
+    }
+    (x == 0 && y == 0 && z == 0) ? cout << "YES\n" : cout << "NO\n";
+
+    return 0;
+}
