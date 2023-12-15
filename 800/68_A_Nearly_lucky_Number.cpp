7552369a87a73693213b6c9c0f5f884b37bf26c1
ahm-fahim <contactahmfahim@gmail.com>
2023-12-15 16:36:47 +0600

    solved

---
+++ 800/68_A_Nearly_lucky_Number.cpp
@@ -0,0 +1,47 @@
+/****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****\
+ |  Problems! Problems! Problems! I Don't Like It, I Avoid.             |
+ |  But Problems Likes Me! I can't Avoid!                               |
+ |                                                                      |
+ |  A.H.M. Fahim Morshed                                                |
+ |                                                                      |
+ |  ```Is a competitive programming enthusiast who is deeply            |
+ |  passionate about coding. Hailing from Bogura, Bangladesh,           |
+ |  his coding journey started at a young age, driven by a              |
+ |  fascination for solving intricate problems.                         |
+ |                                                                      |
+ |  Fahim has excelled in competitive programming, consistently         |
+ |  ranking among the top participants in various                       |
+ |  contests.Beyond competitions, he shares his                         |
+ |  expertise by mentoring and assisting fellow                         |
+ |  programmers on online forums.                                       |
+ |                                                                      |
+ |  His dedication to self-improvement, willingness to embrace new      |
+ |  challenges, and effective teamwork make him a valuable              |
+ |  asset to any project.                                               |
+ |                                                                      |
+ |  Fahim's love for coding, combined with his diverse interests,       |
+ |  reflects his well-rounded personality, making him an inspiring      |
+ |  figure in the programming community at Southeast University.```     |
+ |                                                                      |
+ |  Github   - ahm-fahim                                                |
+ |  Linkedin - https://www.linkedin.com/in/md-fahim-morshed-5b2126233/  |
+ |  Gmail    - contactahmfahim@gmail.com                                |
+\****~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~****/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define endl '\n';
+
+int main()
+{
+    optimize();
+    char ch;
+    int cnt = 0;
+    while (cin >> ch)
+        if (ch == '4' || ch == '7')
+            cnt++;
+    cout << (cnt == 7 || cnt == 4 ? "YES" : "NO");
+
+    return 0;
+}
